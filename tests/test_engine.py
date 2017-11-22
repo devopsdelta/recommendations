8f3a03e4a89cfb402959d3e1c4f9008bc01a8741
sfloam <scottfloam@gmail.com>
2017-11-22 12:50:23 -0500

    Separated bad tests for parsing into seperate tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,5 +1,6 @@
 import unittest
 from engine import Engine
+
 class EngineTestCase(unittest.TestCase):
     def setUp(self):
         self.rec_engine_1 = Engine('{"id":"1","name":"socks","category":"footwear","price":"4.50"}', 1)
@@ -10,31 +11,26 @@ class EngineTestCase(unittest.TestCase):
         self.other_prod_4 = '{"id":"2","name":"broccoli","category":"vegetables","price":".50"}'
         self.other_prod_5 = '{"id":"2","name":"broccoli","category":"vegetables","price":"somethingwrong"}'
 
-
     def tearDown(self):
         self.rec_engine_1 = None
         self.rec_engine_2 = None
 
-    
-    # def test_instantiation(self):
-    #     self.assertRaises(Exception, self.rec_engine_missing_rec_type = Engine(''))
-    #     self.assertRaises(Exception, self.rec_engine_missing_rec_type = Engine())
-
     def test_parseMetaData(self):
         """Testing Parsing of Product Metadata"""
-        #nothing to parse
-        self.assertEquals(self.rec_engine_1.parseMetaData(""), "Missing metadata for one or both products!" )
-        #invalid json
-        self.assertRaises(Exception, self.rec_engine_1.parseMetaData('"id":"1","name":"socks","adf":"footwear"4.50"}'))
-        
-
         #getting valid product metadata
         p1, p2 = self.rec_engine_1.parseMetaData(self.other_prod_1)
-        
         #valid product_1_metadata
         self.assertEquals(p1,{"id":"1","name":"socks","category":"footwear","price":"4.50"})
         #valid product_2_metadata
         self.assertEquals(p2,{"id":"2","name":"shoes","category":"footwear","price":"8.50"})
+
+    def test_nothingToParse(self):
+        """Testing Nothing to Parse"""
+        self.assertEquals(self.rec_engine_1.parseMetaData(""), "Missing metadata for one or both products!" )
+    
+    def test_parsingInvalidJSON(self):
+        """Testing Parsing Invalid JSON"""
+        self.assertRaises(Exception, self.rec_engine_1.parseMetaData('"id":"1","name":"socks","adf":"footwear"4.50"}'))
 
     def test_badRecType(self):
         """Testing Bad Recommendation Types"""
@@ -46,14 +42,12 @@ class EngineTestCase(unittest.TestCase):
 
     def test_getWeight(self):
         """Testing Correct Weights"""
-
         #upsell algo test          
         self.assertEquals(self.rec_engine_1.getWeight(self.other_prod_1),2) 
         self.assertEquals(self.rec_engine_1.getWeight(self.other_prod_2),1) 
         self.assertEquals(self.rec_engine_1.getWeight(self.other_prod_3),1)
         self.assertEquals(self.rec_engine_1.getWeight(self.other_prod_4),0)
 
-   
 if __name__ == '__main__':
     unittest.main()
 
